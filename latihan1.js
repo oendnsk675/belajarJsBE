a8d0bf8d3b616fe87864710638f157c66f525d45
sayidina ahmadal qososyi <58449456+oendnsk675@users.noreply.github.com>
2021-12-11 12:48:21 +0800

    add info

--- latihan1.js
+++ latihan1.js
@@ -1,6 +1,6 @@
 // latihan membuat permainan simple, gunting batu kertas
 // sebelum memulai latihan ketik `npm install` di terminal kalian, fungsi ini akan menginstall node module yang kita butuhkan, penjelasanya besok lebih spesifik pas kita belajar
-// ada beberapa kodingan yang saya buat, dengan alesan biar gak bingung, intinya saya menekankan untuk latihan dasar  kemarin
+// ada beberapa kodingan yang saya buat, dengan alesan biar gak bingung, seperti di no 1, 3, dan 4, intinya saya menekankan untuk latihan dasar  kemarin
 // secara garis besar algoritma simpelnya seperti berikut
 // 1. mengambil nama user
 // 2. membuat list piliha user, bisa pakek array maupun objek
@@ -14,14 +14,16 @@
 
 const prompt = require('prompt-sync')(); // kita memasukkan module /package prompt-scync, mirip pas kita import scaner di java, sebelum kita pakek
 
+console.log(`Hallo selamat datang dalam permainan gunting, batu, kertas, disini temen bisa memilih sesuai urutan pilihannya dari 0, misal jika ingin memilih gunting maka pilih no 0, dst...:)`);
 
 // 1. mengambil nama user
-// const name = prompt('Nama ente siapa?: '); // fungsi ini untuk mengambil inputan user dari terminal, mirip scaner yang ada di java, kemudian di save ke variabel nama bertipe const, (gak bisa diubah)
+const name = prompt('Masukkan nama anda?: '); // fungsi ini untuk mengambil inputan user dari terminal, mirip scaner yang ada di java, kemudian di save ke variabel nama bertipe const, (gak bisa diubah)
 
-// 2. buatlah list pilihan user, bisa memakai array ataupun objek, objek lebih saya suka karna agak panjangan dikit wkwkkw:)
+// 2. buatlah list pilihan user, bisa memakai array, dangan isi urutannya, gunting, batu , kertas
 
 
-// 3. tanyakan ke user mau memilih apa, untuk menanyakan ke user bisa pakek seperti di no 1, tinggal di copas honestly:) masukkan dari user itu berupa no ya temen2, karena misal nanti temen2 membuat list pilihannya itu memakai array jadi kan bisa diakses bosku......, dan tampung pilihan user tersebut ke dalam variabel dengan nama pilihan_user
+// 3. tanyakan ke user mau memilih apa, untuk menanyakan ke user bisa pakek seperti di no 1, tinggal di copas honestly:) masukkan dari user itu berupa no ya temen2, karena misal nanti temen2 membuat list pilihannya itu memakai array jadi kan bisa diakses bosku......, dan tampung pilihan user tersebut ke dalam variabel dengan nama pilihan_user, setelah saya pikir-pikir, saya buatkan saja no3 karna belum saya jelaskan wkwkwk
+pilihan_user  = int(prompt('Masukkan no pilihan anda (0-2): '));
 
 
 // 4. membuat pilihan komputer dengan menggunakan fungsi random dari javascript, fungsi random ini akan menghasilkan angka yang kita inginkan
